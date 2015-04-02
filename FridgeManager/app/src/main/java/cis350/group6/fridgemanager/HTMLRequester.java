b9d6c4f9de1f7682ab24e585c6dea61564991a49
David Liao <danedaworld@gmail.com>
2015-04-02 15:47:04 -0400

    filled in Constants and HTMLRequester

--- FridgeManager/app/src/main/java/cis350/group6/fridgemanager/HTMLRequester.java
+++ FridgeManager/app/src/main/java/cis350/group6/fridgemanager/HTMLRequester.java
@@ -1,5 +1,15 @@
 package cis350.group6.fridgemanager;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.StatusLine;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+
 /**
  *
  * Make HTML requests for querying a database
@@ -7,5 +17,33 @@ package cis350.group6.fridgemanager;
  * Created by root on 4/2/15.
  */
 public class HTMLRequester {
+    private static HTMLRequester instance = null;
 
+    private HttpClient httpclient;
+
+    protected HTMLRequester() {
+        httpclient = new DefaultHttpClient();
+    }
+
+    public String HTMLRequest(String URL) throws IOException{
+        HttpResponse response = httpclient.execute(new HttpGet(URL));
+        StatusLine statusLine = response.getStatusLine();
+        if(statusLine.getStatusCode() == HttpStatus.SC_OK){
+            ByteArrayOutputStream out = new ByteArrayOutputStream();
+            response.getEntity().writeTo(out);
+            String responseString = out.toString();
+            out.close();
+            return responseString;
+        } else{
+            response.getEntity().getContent().close();
+            throw new IOException(statusLine.getReasonPhrase());
+        }
+    }
+
+    public static HTMLRequester getInstance() {
+        if (instance == null) {
+            instance = new HTMLRequester();
+        }
+        return instance;
+    }
 }
